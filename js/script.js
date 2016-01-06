d88cf4707233e7653db15c7f366daf587a3d4527
RSSarikaTyagi <sarika.tyagi@rsystems.com>
2016-01-06 10:28:57 +0530

    remove js files

--- js/script.js
+++ js/script.js
@@ -38,7 +38,6 @@ $(function() {
         var homeHeight= $(window).height();
         $('#overlay-1').height(homeHeight);
     }
-
     setHomeBannerHeight();
 
 ///////////////////////////////
@@ -57,7 +56,6 @@ $('a[href*=#]:not([href=#])').click(function() {
         }
     }
 });
-
 ///////////////////////////////
 // Center Home Slideshow Text
 ///////////////////////////////
@@ -70,203 +68,21 @@ function centerHomeBannerText() {
 }
 
 centerHomeBannerText();
-
 jQuery(window).smartresize(function() {
     setHomeBannerHeight();
     centerHomeBannerText();
 });
-    
 });
 
 $(document).ready(function(){
-    new WOW().init();
-		$("#client-speech").owlCarousel
-		({
-			autoPlay: 3000,
-			navigation : false, // Show next and prev buttons
-			slideSpeed : 700,
-			paginationSpeed : 1000,
-			singleItem:true
-		});
-
-    var setHomeBannerHeight = function(){
-   var homeHeight= $(window).height();
-   $('#overlay-1').height(homeHeight);
-    }
-    setHomeBannerHeight();  
-
-       
-	});
-
-
-
-
-
-$(document).ready(function(){
-
-  var menu = $('#navigation-scroll > .navbar');
-  var origOffsetY = $('#bottom').offset().top;
-
-  function scroll() {
-     if ($(window).scrollTop() > origOffsetY) {
-        menu.addClass('navbar-white');
-     } else {
-        menu.removeClass('navbar-white');
-     }
-  }
-
-  document.onscroll = scroll;
-
-
-
-
-  $('#screenshots-wrap > a').nivoLightbox({effect: 'fadeScale'});
-
+  var setHomeBannerHeight;
+  setHomeBannerHeight = function () {
+    var homeHeight = $(window).height();
+    $('#overlay-1').height(homeHeight);
+  };
+  setHomeBannerHeight();
   $(window).load(function(){
     $('#preloader').fadeOut('slow',function(){$(this).remove();});
   });
-
-
-
-  $("#client-speech").owlCarousel({
-    autoPlay: 3000,
-      navigation : false, // Show next and prev buttons
-      slideSpeed : 700,
-      paginationSpeed : 1000,
-      singleItem: true
-  });
-
-
-  $("#screenshots-wrap").owlCarousel({
-      navigation : false,
-      pagination: true,
-      slideSpeed : 500,
-      items: 4
-  });
-
-
 });
 
-// @media (max-width: 515px){
-//     .rene
-//     {
-//         font-size: 70px;
-//     }
-// }
-// @media (max-width: 405px){
-//     .heading-text h3
-//     {
-//         font-size: 45px;
-//     }
-// }
-// @media (max-width: 350px){
-//     .rene
-//     {
-//         font-size: 50px;
-//     }
-//     .starting-text h2
-//     {
-//         font-size: 35px;
-//     }
-// }
-// /* 
-
-// .shadow-team
-// {
-//     background-color: #626262;
-//     border-radius: 50%;
-//     content: "";
-//     height: 14px;
-//     left: 14px;
-//     position: relative;
-//     top: 3px;
-//     width: 193px;
-// }
-
-
-
-//  */
-
-// .ion-ios7-arrow-down {
-//     color: #fff;
-//     font-size: 3em;
-// }
-
-
-// .navbar-default .navbar-brand:hover, .navbar-default .navbar-brand:focus {
-//     color: #FFFFFF;
-// }
-
-// .btn-primary {
-//     padding-top: 14px;
-//     border-color: #39c;
-// }
-// input[type="submit"]{
-//     padding-top: 8px;
-//     border-color: #39c;
-// }
-// input[type="submit"]:hover{
-//     color: #fff;
-//     background-color: #286090;
-//     border-color: #204d74;
-// }
-// .pagination>.active>a, .pagination>.active>span, .pagination>.active>a:hover, .pagination>.active>span:hover, .pagination>.active>a:focus, .pagination>.active>span:focus {
-//     background-color: #3acab1;
-//     border-color: #3acab1;
-// }
-
-// .pagination {
-//     border-radius: 0;
-// }
-
-// .pagination>li:first-child>a, .pagination>li:first-child>span {
-//     border-top-left-radius: 0;
-//     border-bottom-left-radius: 0;
-// }
-
-// .pagination>li:last-child>a, .pagination>li:last-child>span {
-//     border-top-right-radius: 0;
-//     border-bottom-right-radius: 0;
-// }
-
-// .navbar-fixed-top, .navbar-fixed-bottom {
-//     position: fixed;
-//     z-index: 5;
-// }
-
-// .btn-default:hover, .btn-default:focus, .btn-default:active, .btn-default.active, .open>.dropdown-toggle.btn-default {
-//     color: #FFF !important;
-//     background-color: #39c;
-//     border-color: #39c;
-// }
-
-// .pagination>li>a, .pagination>li>span {
-//     color: #16a085;
-// }
-
-// #port-items .col-md-4 {
-//     padding: 0;
-// }
-
-// .input-group-addon {
-//     border-radius: 0;
-// }
-
-// @media (max-width: 712px) {
-
-// }
-
-// @media (max-width: 400px) {
-//     #port-items .col-xs-6 {
-//         width: 100%;
-//     }
-
-//     .navbar-fixed-top, 
-//     .navbar-fixed-bottom {
-//         position: static;
-//     }
-
-//     #portfolio .btn {
-//         margin-bottom: 10px;
-//     }
-// }
